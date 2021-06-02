863bfe744a183d2886d8547d863b39646f02ece9
Facundo Paez <41223086@comunidadort.edu.ar>
2021-06-02 13:29:52 -0300

    Se hacen parcialmente las vistas de Search, Login y SignUp

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,44 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 function Search() {
+    const [text, setText] = useState('');
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Search!</Text>
+            <TextInput
+                style={styles.input}
+                value={text}
+                placeholder={'Harry Potter, Lord of the Rings...'}
+                onChangeText={(text) => setText(text)}
+            />
+
+            <TouchableOpacity onPress={() => alert('Buscando...')} style={styles.button}>
+                <Text style={styles.buttonText}>Buscar</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        height: 50,
+        margin: 22,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5,
+        width: 280,
+    },
+    button: {
+        backgroundColor: "#5865F2",
+        paddingVertical: 15,
+        paddingHorizontal: 70,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    }
+});
 
 export default Search;
