576b331b2ff32c8389c398ced885e2d1a726c6e7
MarceloA <marcelo.a.altamura@gmail.com>
2021-06-24 19:00:04 -0300

    info en el home

--- components/Home.js
+++ components/Home.js
@@ -8,6 +8,28 @@ function Home() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#4A5156' }}>
             <Text style={{ color: '#E2EAE9' }}>Bienvenido, {dataUsuario.usuario.nombre}!</Text>
+            <View>
+                <Text style={{alignItems: 'left', textAlign: 'Left',color: '#E2EAE9' }}>
+                    Funcionalidades terminadas:
+                </Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Registro </Text>
+
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Iniciar sesión</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Cerrar sesión</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Entrar a Tu Perfil</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Ver tus Peliculas</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Buscar Usuario</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Buscar Película</Text>
+
+
+                <Text style={{alignItems: 'left', textAlign: 'Left',color: '#E2EAE9'  }}>
+                    Funcionalidades pendientes:
+                </Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9'  }}>-Entrar a perfil de Usuario Buscado</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9' }}>-Entrar a perfil de Película buscada</Text>
+                <Text style={{ textAlign: 'cemter', justifyContent: 'center', flexDirection: 'column',color: '#E2EAE9' }}>-Dejar reseña y puntuación</Text>
+
+            </View>
         </View>
     );
 }
