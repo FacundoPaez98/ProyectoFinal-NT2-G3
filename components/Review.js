ef4a81a57fd2c86c3873d3b2c5d75e8e5b8eb06a
MarceloA <marcelo.a.altamura@gmail.com>
2021-06-24 04:33:24 -0300

    Reviews

--- components/Review.js
+++ components/Review.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
-function Resenias() {
-    return (
-        <View style={{ backgroundColor: '#4A5156' }}>
-            <Text style={{color: '#E2EAE9'}}>Reseñas!</Text>
+function Review(props) {
+
+    const Item = ({ texto }) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{Texto}</Text>
         </View>
     );
+
+    return (
+        <ScrollView>
+            {
+                props.data.map(function (item) {
+                    return (
+                        <Item username={item.texto} />
+                    )
+                })
+            }
+        </ScrollView>
+    )
 }
+const styles = StyleSheet.create({
+    item: {
+        alignItems: "center",
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: '#676C6F',
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 15,
+        color: '#E2EAE9',
+    },
+});
 
 export default Resenias;
